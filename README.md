5450a4f22b09592d95f3900c8561b02a4512eae8
Vincent Wu <wuv21@uw.edu>
2016-05-07 21:56:42 -0700

    added rough draft of documentation and modified script files

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # info474-software-design
+
+## CCChart (Circular Calendar Chart)
+Circular Calendar Charts are good for representing monthly data across multiple years. An example is shown below representing random values for each month from 2000 to 2005.
+
+![Example of CCChart](img/example.png)
+
+## General usage
+This chart requires a data-join with an array of objects. Below is an example of the object:
+
+```javascript
+{
+    month: "January",
+    year: 2005,
+    value: 100
+}
+```
+
+A few notes on the object:
+- `month` must be a string with first letter capitalized
+- `year` must be a number
+- `value` must be a number
+
+## Chart functions
+
+\# *CCChart*()
+> Constructs a Circular Calendar Chart and returns itself.
+
+\# *CCChart*.width([num])
+> If no argument given, returns current chart width (default is `960` px).
+> If *num* given, sets chart width to *num* and returns current CCChart.
+
+\# *CCChart*.height([num])
+> If no argument given, returns current chart height (default is `800` px).
+> If *num* given, sets chart height to *num* and returns current CCChart.
+
+\# *CCChart*.colorScale([colors])
+> If no argument given, returns current color scale (default is `["#deebf7", "#084594"]`).
+> If *colors* given, sets color scale to *colors* and returns current CCChart. The *color* must be set as an array of CSS string representations of colors in the format: `[minColor,[optionalColorIntervals, maxColor]`.
+
+\# *CCChart*.arcConstraints([constraints])
+> If no argument given, returns current arc constraints (default is `[width *.08, width / 3.5]`).
+> If *constraints* given, set arc radius constraints to *constraints* and returns current CCChart. *constraints* must be set as an array of two numbers in the format: `[minRadius, maxRadius]`. The two numbers must be within the current height and width and is greater than 0. The numbers represent the radial distance from the center of the circle.
+
+\# *CCChart*.hoverColor([color])
+> If no argument given, returns current path hover color (default is `"#EB7F00"`).
+> If *color* given, sets hover color to *color* and returns current CCChart. *color* must be set as a CSS acceptable color string. This setting represents the color that appears on mouse hover events over the individual arcs.
+
+\# *CCChart*.arcStrokeWidth([num])
+> If no argument given, returns current stroke width in drawing the paths (default is `2`).
+> If *num* given, sets path stroke width to *num* and returns current CCChart. Path stroke width is proportional to the gaps between the individual arc widths. *num* must be set as a number greater than 0.
--- js/CCChart.js
+++ js/CCChart.js
@@ -4,109 +4,143 @@
 // http://prcweb.co.uk/lab/energy/ - example of circular visualization
 // http://bl.ocks.org/cmdoptesc/6226150 - concentric circle visualization
 
-
 function CCChart() {
-     var width = 960;
-     var height = 800;
-     var circle = 2 * Math.PI;
+    var width = 960;
+    var height = 800;
+    var circle = 2 * Math.PI;
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var colorScale = ["#deebf7", "#084594"];
+    var arcConstraints = [width *.08, width / 3.5];
+    var hoverColor = "#EB7F00";
+    var arcStrokeWidth = 2;
 
+    function my(selection) {
+        selection.each(function(data) {
 
-     function my(selection) {
-         selection.each(function(data) {
-             //console.log(data);
+            var svg = d3.select(this).selectAll('svg').data(data);
 
-             var svg = d3.select(this).selectAll('svg').data(data);
-             var gEnter = svg.enter().append('svg')
-                 .attr('width', width)
-                 .attr('height', height);
+            var gEnter = svg.enter().append('svg')
+                .attr('width', width)
+                .attr('height', height);
 
-             var g = gEnter.append('g')
-                 .attr('transform', 'translate(' + width/3 + ',' + height/2.5 + ')');
+            svg.exit().remove();
 
-             var textDisplay = svg.append('text')
-                 .attr('transform', 'translate(' + (width/3 + 325) + ',' + height/2.5 + ')')
-                 .attr('width', 400)
-                 .attr('font-size', 20)
-                 .attr('fill', '#000')
-                 .text('Hover over individual sector');
+            gEnter.append('g')
+                .attr('transform', 'translate(' + width/3 + ',' + height/2.5 + ')');
+            var g = svg.selectAll('g');
 
-             var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-             var angleYear = [];
-             for (var i = 1; i <= 12; i++) {
-                 angleYear.push({
-                     startAngle: circle / 12 * (i-1),
-                     endAngle: circle / 12 * i
-                 });
-             }
+            var textDisplay = gEnter.append('text')
+                .attr('transform', 'translate(' + (width * (2/3)) + ',' + height/2.5 + ')')
+                .attr('font-size', 20)
+                .attr('fill', '#000')
+                .text('Hover over individual sector');
 
-             var minYear = d3.min(data, function(d) {return d.year});
-             var maxYear = d3.max(data, function(d) {return d.year});
+            var accessedData = data[0];
 
-             var minValue = d3.min(data, function(d) {return d.value});
-             var maxValue = d3.max(data, function(d) {return d.value});
+            var minYear = d3.min(accessedData, function(d) {return d.year});
+            var maxYear = d3.max(accessedData, function(d) {return d.year});
 
-             var monthScale = d3.scale.ordinal().domain(months).rangeBands([0, circle]);
-             var yearScale = d3.scale.linear().domain([minYear, maxYear]).range([80, 300]);
-             var valueScale = d3.scale.linear().domain([minValue, maxValue]).range(["#deebf7", "#084594"]);
+            var minValue = d3.min(accessedData, function(d) {return d.value});
+            var maxValue = d3.max(accessedData, function(d) {return d.value});
 
-             var createArc = d3.svg.arc()
-                 .innerRadius(function(d) {return yearScale(d.year - 1)})
-                 .outerRadius(function(d) {return yearScale(d.year)})
-                 .startAngle(function(d) {return monthScale(d.month)})
-                 .endAngle(function(d) {return monthScale(d.month) + circle / 12});
+            var monthScale = d3.scale.ordinal().domain(months).rangeBands([0, circle]);
+            var yearScale = d3.scale.linear().domain([minYear, maxYear]).range(arcConstraints);
+            var valueScale = d3.scale.linear().domain([minValue, maxValue]).range(colorScale);
 
-             var arcs = g.selectAll('path').data(data[0]);
+            var createArc = d3.svg.arc()
+                .innerRadius(function(d) {return yearScale(d.year - 1)})
+                .outerRadius(function(d) {return yearScale(d.year)})
+                .startAngle(function(d) {return monthScale(d.month)})
+                .endAngle(function(d) {return monthScale(d.month) + circle / 12});
 
-             arcs.enter().append('path')
-                 .attr('class', 'data-arcs')
-                 .attr('id', function(d) {console.log(d); return "arc-" + d.year + d.month})
-                 .style('fill', function(d) {return valueScale(d.value)})
-                 .attr('data-toggle', 'tooltip')
-                 .attr('stroke', '#fff')
-                 .attr('stroke-width', 3)
-                 .attr('d', createArc)
-                 .attr('title', function(d) {return d.year + " " + d.month})
-                 .on('mouseover', function(d) {
-                     console.log(d);
-                     textDisplay.text(d.month + ' ' + d.year + ': ' + d.value);
-                     d3.select(this)
-                         .style('fill', '#EB7F00');
-                 })
-                 .on('mouseout', function(d) {
-                     textDisplay.text('Hover over individual sector');
-                     d3.select(this)
-                         .style('fill', function(d) {return valueScale(d.value)});
+            var arcs = g.selectAll('path').data(accessedData);
 
-                 });
+            arcs.enter().append('path')
+                .attr('class', 'data-arcs')
+                .attr('id', function(d) {return "arc-" + d.year + d.month})
+                .attr('data-toggle', 'tooltip')
+                .style('fill', '#DDD')
+                .attr('stroke', '#DDD')
+                .attr('stroke-width', arcStrokeWidth)
+                .attr('d', createArc)
+                .attr('title', function(d) {return d.year + " " + d.month + ": " + d.value})
+                .on('mouseover', function(d) {
+                    textDisplay.text(d.month + ' ' + d.year + ': ' + d.value);
+                    d3.select(this)
+                        .style('fill', hoverColor);
+                })
+                .on('mouseout', function(d) {
+                    textDisplay.text('Hover over individual sector');
+                    d3.select(this)
+                        .style('fill', function(d) {return valueScale(d.value)});
+                });
 
-             var monthLabels = g.append("text");
+            arcs.exit().remove();
 
-             months.forEach(function(month) {
-                 monthLabels.append("textPath")
-                     .attr("text-anchor", "middle")
-                     .attr("dy", 50)
-                     .attr("startOffset", "20%")
-                     .attr("stroke", "#ccc")
-                     .attr("stroke-width", 0)
-                     .attr("xlink:href", '#arc-' + maxYear + month)
-                     .text(month);
-             })
-         });
-     }
+            arcs.transition()
+                .duration(2000)
+                .style('fill', function(d) {return valueScale(d.value)})
+                .attr('d', createArc);
 
-     my.width = function(val) {
-         if (!arguments.length) return width;
+            var monthLabels = gEnter.append("text")
+                .attr('transform', 'translate(' + width/3 + ',' + height/2.5 + ')');
 
-         width = val;
-         return my;
-     };
+            months.forEach(function(month) {
+                monthLabels.append("textPath")
+                    .attr("text-anchor", "middle")
+                    .attr("dy", 50)
+                    .attr("startOffset", "20%")
+                    .attr("stroke", "#ccc")
+                    .attr("stroke-width", 0)
+                    .attr("xlink:href", '#arc-' + maxYear + month)
+                    .text(month);
+            });
 
-     my.height = function(val) {
-         if (!arguments.length) return height;
+            svg.exit().remove();
+        });
+    }
 
-         height = val;
-         return my;
-     };
+    my.width = function(val) {
+        if (!arguments.length) return width;
+
+        width = val;
+        return my;
+    };
+
+    my.height = function(val) {
+        if (!arguments.length) return height;
+
+        height = val;
+        return my;
+    };
+
+    my.colorScale = function(val) {
+        if (!arguments.length) return colorScale;
+
+        colorScale = val;
+        return my;
+    };
+
+    my.arcConstraints = function(val) {
+        if (!arguments.length) return arcConstraints;
+
+        arcConstraints = val;
+        return my;
+    };
+
+    my.hoverColor = function(val) {
+        if (!arguments.length) return hoverColor;
+
+        hoverColor = val;
+        return my;
+    };
+
+    my.arcStrokeWidth = function(val) {
+        if (!arguments.length) return arcStrokeWidth;
+
+        arcStrokeWidth = val;
+        return my;
+    };
 
     return my;
- }
+}
